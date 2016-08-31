69998d2f022513ec3ae24102736239f263adf394
Kahlil Dozier <kahlildozier@kahlils-macbook-pro-2.local>
2016-08-31 12:34:01 -0400

    Initial Commit

---
+++ KinematicsSolver/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KinematicsSolver
+//
+//  Created by Kahlil Dozier on 8/31/16.
+//  Copyright © 2016 Kahlil Dozier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
